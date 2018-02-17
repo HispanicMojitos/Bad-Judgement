674e57abd7e3af941f52c86fc9401bbb4c5bfe31
Xinart <36197906+Xinart@users.noreply.github.com>
2018-02-17 13:45:31 +0100

    Changement du 17/02/2018
    3 scripts ajouté
    3 assets de sons ajouté
    Une scene ajouté

---
+++ Bad Judgement/Assets/Scripts/MusicBackground.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using UnityEngine.Audio;
+using System.Collections;
+
+public class MusicBackground : MonoBehaviour {
+
+    public AudioClip musicClip;
+    public AudioSource musicSource;
+    
+	void Start () {
+        musicSource = GetComponent<AudioSource>(); // Initialise le son avec l'emmeteur respectif 
+        musicSource.volume = 0.05f;
+	}
+	
+	void Update () {
+        if (!musicSource.isPlaying) // Joue le son et le repete 
+        {
+            musicSource.clip = musicClip;
+
+            musicSource.Play();
+        }
+	}
+}
