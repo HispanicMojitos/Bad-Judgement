f082a96753db99cad7baa7e44ffa1b3656f5080e
BelgianSoviet <andrewsleruth@gmail.com>
2018-02-21 22:31:26 +0100

    Huge update with better shooting
    Also added the DevDocs folder for you guys to add your docs too.

---
+++ Bad Judgement/Assets/Scripts/Target.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Target : MonoBehaviour
+{
+    // Now we need a target script to recieve damage and other stuff
+    public float health = 50f;
+
+    public void TakeDamage(float amount)
+    {
+        health -= amount;
+        if (health <= 0f) // we create a script that destroys an object when its health is equal or lower than 0
+        {
+            Die();
+        }
+    }
+
+    void Die()
+    {
+        Destroy(gameObject);
+    }
+
+}
