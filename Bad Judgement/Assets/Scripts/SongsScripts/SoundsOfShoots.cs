756b08d6698d5d7d80d79e09d0bb3823334b3395
Xinart <36197906+Xinart@users.noreply.github.com>
2018-02-21 23:09:36 +0100

    Scipts soundChanges
    I've added new sounds, added another folder in sounds assets to manage better them
    I've improoved 2 scripts :  the onne for making sonds when we shoot and the second one for making dedicated sounds when we jump, and i've add comments ... Andrews now when you walk you must be able to hear the steps sounds

--- Bad Judgement/Assets/Scripts/SongsScripts/SoundsOfShoots.cs
+++ Bad Judgement/Assets/Scripts/SongsScripts/SoundsOfShoots.cs
@@ -1,53 +1,78 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+// /!\ A OPTIMISER MARTIN /!\ ATTENTION CE SCRIPT VA BEAUCOUP CHANGER, NOTTEMMENT LORSQU'ON VA METTRE PLUSIEURS ARME, JY AI BEAUCOUP REFLECHI ET JE VERRAI AVEC VOUS CAR IL FAUT ADAPTER LE SCRIPTS EN FONCTION DES ARMES
+public class SoundsOfShoots : MonoBehaviour
+{
+    public AudioClip shootSound; // Son de tirs du AK47
+    public AudioSource machineGun; // Source des sons = 1K47
+    public AudioClip endsong; // Son de fin apres les tirs
+    public AudioClip reload; // son de rechargement
 
-public class SoundsOfShoots : MonoBehaviour {
-    public AudioClip shootSound;
-    public AudioSource machineGun;
-    private int indice = 1;
-    private float timeSound;
     
-    [Range(0f,1f)]
-    public float volume = 0.2f;
+    private float timeSound; // temps du son de tir joué au fur et a mesur que l'on presse le bouton
+    private bool sonJoué = false; // pour l'algorithme sache si le son est joué ou pas
+    private int indice2 = 1; // Indice n°2 utilisé pour un fonctionnement de l'algorithme     !! EXPLIQUé PLUS BAS !! 
+    private int indice1 = 1; // Indice n°1 utilisé pour un fonctionnement de l'algorithme
 
-    [Range(1, 5)]
+    [Range(0f,1f)] // Permet de regler le son
+    public float volume = 0.2f; 
+    [Range(1, 5)] // Permet de regler la 'vitesse' du son ANDREWS TEST sur 3 CA FAIS UN SON DE MINIGUN HAHA
     public int intensité = 1;
 
 	// Use this for initialization
 	void Awake  () {
-        machineGun.clip = shootSound;
-        machineGun.volume = volume;
-        machineGun.pitch = intensité;
-        machineGun.spatialBlend = 1f;
-        machineGun.spatialize = true;
-        timeSound = shootSound.length;
+        machineGun.clip = shootSound; // implémente le son de tir a la source (AK47)
+        machineGun.volume = volume; // implémente l'intensité
+        machineGun.pitch = intensité; // implément l'intensité
+        machineGun.spatialBlend = 0.8f; // permet d'avoir un son 'réaliste'
+        timeSound = shootSound.length; // regle le temps du son de tirs 
 	}
 	
 	// Update is called once per frame
 	void Update ()
     {
-        if(Input.GetButtonDown("Fire1"))
+        if(Input.GetButtonDown("Fire1")) // Si la touche pour tirer est cliqué, initialise des variables nécéssaire pour l'algorithme de fonctionnement 
         {
-            indice = 0;
-            timeSound = shootSound.length;
+            indice1 = 0;
+            timeSound = shootSound.length; // intialise le temps de son
+            machineGun.clip = shootSound; // intitialise le son de tir des que l'on clique, logique
+            sonJoué = true;
         }
 
-		if(Input.GetButton("Fire1") && indice == 0)
+
+		if(Input.GetButton("Fire1") && indice1 == 0)  // Si la touche pour tirer est préssé et que le son doit continuer de jouer (indice1) , on joue le son !! => ALGORITHME
         {
-            timeSound = timeSound - Time.deltaTime;
-            if (!machineGun.isPlaying)
+            indice2 = 1; // Empeche une boucle infinie du son de fin, croyez moi IL FAUT laisser ca la x) => ALGORITHME
+            timeSound = timeSound - Time.deltaTime; // permet de decrementer le  temps du son actuel du tir en fonction du temps qui passe
+            if (!machineGun.isPlaying) // si le son de tir n'est pas détécté, on va le jouer quoi !!!
             {
-                if (timeSound > 0)
+                if (timeSound > 0) // si le temp de son qui reste est supérieur à 0, on le jour
                     machineGun.Play();
-                else if(timeSound <= 0)
-                    indice = 1;
+                else if(timeSound <= 0) // si le temp de son qui reste est inferieur a Zero, on increment l'indice1 pour qque le son ne se fasse pas en boucle  => ALGORITHME
+                    indice1 = 1;
             }
+        }
+        else if(Input.GetKeyDown(KeyCode.R) && !machineGun.isPlaying) // Si la touche R est préssée pour tirer, le son de recharge se fait !!
+        {
+            machineGun.clip = reload; // On implemente le son de rechargement
+            machineGun.Play(); // On joue le son de rechargement
         }
         else
         {
-            machineGun.Stop();
+            if (sonJoué == true) // permet d'acceder a la condition suivante et de decrementer le  temps du son actuel du tir
+            {
+                indice2 = 0; // Permet d'acceder a la condition suivante 
+                timeSound = timeSound - Time.deltaTime; // Decrementation du temps de son passé
+            }
+            if (indice2 == 0 && timeSound > endsong.length) // Permet de jouer le son de fin 
+            {
+                machineGun.Stop(); // On arrete le son de tir
+                machineGun.clip = endsong; // On implement le son de fin de tir (pour faire plus réaliste et pas faire un son coupé quoi
+                machineGun.Play(); // On joue le son de fin de tir
+                sonJoué = false; // Le son ne sera plus joué apres
+                indice2 = 1;
+            }
         }
-
 	}
 }
--- Bad Judgement/Assets/Scripts/SongsScripts/StepFootSoundEffect.cs
+++ Bad Judgement/Assets/Scripts/SongsScripts/StepFootSoundEffect.cs
@@ -1,13 +1,18 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+// /!\ A REVENIR DESSUS POUR OPTIMISER MARTIN, AMELIORER LES SONS DE PAS SI POSSIBLE , AJOUTER LES SONS DATTERISAGE DE SAUTS ET TROUVER UN ASSETS DE SAUT POUR LA VOIX SI POSSIBLE
 public class StepFootSoundEffect : MonoBehaviour
 {
-    public AudioClip sonDePasSurRue; //Initialisation des sons
+    public AudioClip sonDePasSurRue; //Initialisation des sons de pas 
     public AudioClip sonDePasSurBois;
     public AudioClip sonDePasSurterre;
+    public AudioClip jumpOnGrass; // Initialisation de sons de Jump
+    public AudioClip jumpOnStreet;
+    public AudioClip jumpOnWood;
     public AudioSource personnage;
+    public AudioSource piedjumpPersonnage;
+
     [Range(0f, 1f)] // Permet de regler le volume via un bouton de reglage dans Unity
     public float volumeDesSonsDePas = 0.2F; 
     [Range(1, 3)] // Permet de regler l'intensité via un bouton de reglage dans Unity
@@ -16,31 +21,52 @@ public class StepFootSoundEffect : MonoBehaviour
     private void OnCollisionEnter(Collision collision) // Permet d'evaluer le son a jouer en fonction du type de sol rencontré /!\ On a besoin d'un rigibody et d'une box Colider /!\
     {
         if (collision.transform.tag == "Wood")
+        {
             personnage.clip = sonDePasSurBois; // Ici le son va alors devenir celui d'un bruit de pas sur le bois, pour les autres ca va jouer les son que l'on aura alors importé aussi lorsque la surface change
+            piedjumpPersonnage.clip = jumpOnWood; // Pareil pour les jumps 
+        }
         else if (collision.transform.tag == "Street")
+        {
             personnage.clip = sonDePasSurRue;
+            piedjumpPersonnage.clip = jumpOnStreet;
+        }
         else if (collision.transform.tag == "Grass")
+        {
             personnage.clip = sonDePasSurterre;
+            piedjumpPersonnage.clip = jumpOnGrass;
+        }
     }
 
     // Use this for initialization
-    void Start () {
+    void Start ()
+    {
         personnage.volume = volumeDesSonsDePas;  // Regle le volume des pas
+        piedjumpPersonnage.volume = 0.5f;
         personnage.spatialBlend = 1f;  // Permet d'avoir les effet de son en 3D (on entend les bruits de pas de gauche vers la gauche, de droite vers la droite, etc)
+        piedjumpPersonnage.spatialBlend = 1f;
         personnage.pitch = intensité; // Regle l'intensité du son
-        personnage.spatialize = true;
+        piedjumpPersonnage.pitch = intensité;
     }
 	
 	// Update is called once per frame
-	void Update () {
-        if ((Input.GetKey(KeyCode.Q) || Input.GetKey(KeyCode.S) || Input.GetKey(KeyCode.Z) || Input.GetKey(KeyCode.D)) && !Input.GetKey(KeyCode.Space))
+	void Update ()
+    {
+        if ((Input.GetKey(KeyCode.Q) || Input.GetKey(KeyCode.S) || Input.GetKey(KeyCode.Z) || Input.GetKey(KeyCode.D)) || Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.W) && !Input.GetKey(KeyCode.Space)) // permet d'evite 
         {
-            if (!personnage.isPlaying) //Permet de jouer les bruits de pas si les touche "S","Q","Z","D"
+            if (!personnage.isPlaying && !piedjumpPersonnage.isPlaying && !Input.GetKey(KeyCode.Space)) //Permet de jouer les bruits de pas si les touche "S","Q","Z","D"
             {
-                    personnage.Play();
+                    personnage.Play(); // Joue le son attaché a la source qu'est le personnage
+            }
+            else if (Input.GetKeyDown(KeyCode.Space)) // Si la touche espace est appuyé pour sauter, les sons de saut von se faire
+            {
+                piedjumpPersonnage.Play(); // Joue le son attaché a la source que sont les pieds du personnages
             }
         }
+        else if(Input.GetKeyDown(KeyCode.Space)) // Si la touche espace est appuyé pour sauter, les sons de saut von se faire
+        {
+            piedjumpPersonnage.Play(); // Joue le son attaché a la source que sont les pieds du personnages
+        }
         else
-            personnage.Stop();
+            personnage.Stop(); // ne joue pas le son de pas si on ne bouge pas quoi, normal x)
 	}
 }
