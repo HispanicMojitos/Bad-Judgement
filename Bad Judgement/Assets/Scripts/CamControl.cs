446663464f26587dda69a476fde9eb4025d2f62e
Terraxel99 <36198830+Terraxel99@users.noreply.github.com>
2018-02-23 18:28:07 +0100

    Movement and camera update
    Clarified my code by adding methods and regions. Added properties for later and for shooting script.
    
    Working on clamping the camera vertically but I didn't find a good way to do it.

--- Bad Judgement/Assets/Scripts/CamControl.cs
+++ Bad Judgement/Assets/Scripts/CamControl.cs
@@ -4,49 +4,81 @@ using UnityEngine;
 
 public class CamControl : MonoBehaviour
 {
+    #region Members
+
     private Camera cam; //Camera (it's a children component, this script has to be applied to the player !!!!!)
-    public Transform target;
 
-    //private float minVertical = -55.0F;
-    //private float maxVertical = 55.0F; //Maximum head angle is 55 degrees. Modifiable here.
+    private float minVertical = -55.0F;
+    private float maxVertical = 60.0F;//Maximum head angle is 55 degrees. Modifiable here.
 
-    private float horizontalSensitivity = 6.0F;
-    private float verticalSensitivity = 10.0F;
+    #endregion
 
-    public bool isVerticalAxisInverted = false; //FOR YOU LERUTH  //WIP
+    #region Properties
+
+    public bool isVerticalAxisInverted { get; private set; } //For you LERUTH
+
+    public float horizontalSensitivity { get; set; }
+    public float verticalSensitivity { get; set; }
+
+    #endregion
+
+    #region Start and Update
 
     // Use this for initialization
-    void Start ()
+    void Start()
     {
         cam = Camera.main; //Starting with the main cam which is the FPS one
                            //As project I'd like to put a second cam which could be changed to TPS if we press a button
+    
+        //Initializing properties :
+        this.isVerticalAxisInverted = false;
+        this.horizontalSensitivity = 6.0F;
+        this.verticalSensitivity = 10.0F;
+    }
 
-        //Has to implement invert Y-axis
-	}
-	
 
-	// Update is called once per frame
-	void Update ()
+    // Update is called once per frame
+    void Update()
     {
         float yGameAxis = Input.GetAxis("Mouse X"); //The horizontal (X) mouse axis matches with the ingame y rotation axis
         float xGameAxis = Input.GetAxis("Mouse Y"); //The vertical (Y) mouse axis matches with the ingame x rotation axis
 
-        yGameAxis *= verticalSensitivity;
-        xGameAxis *= horizontalSensitivity; //Appliying sensitivity to the camera axes.
-
-        if(isVerticalAxisInverted) this.cam.transform.Rotate(-xGameAxis, 0F, 0F); 
-        else this.cam.transform.Rotate(xGameAxis, 0F, 0F); //Vertically rotating (applied to the cam)
-
-        this.transform.Rotate(0F, yGameAxis, 0F); //Horizontal rotate (applied to the player)
+        if (yGameAxis != 0) MoveCamHoriz(yGameAxis);
+        if (xGameAxis != 0) MoveCamVertically(xGameAxis);
+        //If the player moves his mouse to rotate camera we proceed to moving camera.
 
         if (Input.GetKey(KeyCode.LeftAlt) && Input.GetKeyDown(KeyCode.Mouse0)) this.InvertAxis();
         //If LeftAlt + LeftClick => Inverting Y axis
     }
 
-    public void InvertAxis()
+    #endregion
+
+    #region Cam Methods
+
+    private void InvertAxis()
     {
-        if(this.isVerticalAxisInverted) this.isVerticalAxisInverted = false;
+        if (this.isVerticalAxisInverted) this.isVerticalAxisInverted = false;
         else this.isVerticalAxisInverted = true;
     }
+
+    private void MoveCamVertically(float xGameAxis)
+    {
+        xGameAxis *= horizontalSensitivity; //Appliying sensitivity to the horizontal camera axis.
+
+        //WIP :
+        //Clamping vertical rotation of the camera betw. limits
+
+        if (isVerticalAxisInverted) this.cam.transform.Rotate(-xGameAxis, 0F, 0F);
+        else this.cam.transform.Rotate(xGameAxis, 0F, 0F); //Vertically rotating (applied to the cam)
+    }
+
+    private void MoveCamHoriz(float yGameAxis)
+    {
+        yGameAxis *= verticalSensitivity; //Applying vertical sensitivity
+
+        this.transform.Rotate(0F, yGameAxis, 0F); //Horizontal rotate (applied to the player)
+    }
+
+    #endregion
 
 }
--- Bad Judgement/Assets/Scripts/Movement.cs
+++ Bad Judgement/Assets/Scripts/Movement.cs
@@ -4,68 +4,106 @@ using UnityEngine;
 
 public class Movement : MonoBehaviour
 {
-    public float forwardSpeed;
-    public float backwardSpeed;
-    public float sideSpeed; 
-    public float verticalSpeed;
-    public float strafeSpeed; //We'll be able to strafe fast. => WIP (2.88 KMH).
+    #region Members
 
-    protected new Rigidbody rigidbody = new Rigidbody(); 
+    private float forwardSpeed;
+    private float backwardSpeed;
+    private float sideSpeed;
+    private float verticalSpeed;
+    private float strafeSpeed; //We'll be able to strafe fast. => WIP (2.88 KMH).
+
+    protected new Rigidbody rigidbody = new Rigidbody();
     private CharacterController charCtrl = new CharacterController();
 
+    #endregion
+
+    #region Properties
+
+    public bool characterIsMoving { get; private set; }
+    public bool characterIsJumping { get; private set; }
+
+    #endregion
+
+    #region Start and Update
 
     // Use this for initialization
     void Start()
     {
-        Cursor.lockState = CursorLockMode.Locked;
-
         //These speeds are based on my real tests and are in KMH :
-
         forwardSpeed = 4.2F;
         backwardSpeed = (0.66F * forwardSpeed); //After real tests, reverse speed is 2/3 times of forward speed.
-        sideSpeed = 2.15F; 
+        sideSpeed = 2.15F;
         verticalSpeed = 4.5F;
 
+        //Getting components :
         rigidbody = GetComponent<Rigidbody>();
         charCtrl.GetComponent<CharacterController>(); //We search the rigidbody and the charController in the player
-    }
-	
 
-	// Update is called once per frame
-	void Update()
+        //To be moved later :
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+
+    // Update is called once per frame
+    void Update()
     {
+        //Checking for Ground Moving :
+
         float xAxis = Input.GetAxis("Horizontal") * sideSpeed * Time.deltaTime;
         float zAxis = Input.GetAxis("Vertical") * Time.deltaTime;
 
-        if (zAxis < 0) zAxis *= backwardSpeed;
-        else zAxis *= forwardSpeed; //Forward speed != than backward speed
-
-        Vector3 movement = new Vector3(xAxis, 0F, zAxis);
-        //X is the strafe and Z is forward/backward
-        this.transform.Translate(movement);
+        if (zAxis != 0 || xAxis != 0) //If the player is moving :
+        {
+            Move(zAxis, xAxis); //We're moving on X and Z
+            characterIsMoving = true;
+        }
+        else characterIsMoving = false; 
 
         //========================================================================================
+
+        //Checking for Jump :
 
         float yAxis = Input.GetAxis("Jump"); //Jump axis (space key)
-        Jump(yAxis); //Move vertically. You'll notice that the character has to be on the ground to jump.
+
+        if (yAxis != 0)
+        {
+            Jump(yAxis);
+            characterIsJumping = true;
+        }
+        else characterIsJumping = false;
 
         //========================================================================================
 
+        //To be moved :
         if (Input.GetKeyDown(KeyCode.Escape)) Cursor.lockState = CursorLockMode.None;
         if (Cursor.lockState == CursorLockMode.None && Input.GetKey(KeyCode.Mouse0)) Cursor.lockState = CursorLockMode.Locked;
     }
-    
 
+    #endregion
+
+    #region Moving Methods
 
     private void Jump(float yAxis)
     {
         yAxis = yAxis * verticalSpeed; //Affecting vertical jump speed and consider machine responsiveness
-
         Vector3 moveVertical = new Vector3(0F, yAxis, 0F); //Creating the vector that contains the vertical !=ce
         moveVertical *= Time.deltaTime; //Machine responsiveness
 
         this.transform.Translate(moveVertical); //Making the jump
         //The character will be automatically brought back to the ground due to gravity.
     }
+
+    private void Move(float zAxis, float xAxis)
+    {
+        if (zAxis < 0) zAxis *= backwardSpeed;
+        else zAxis *= forwardSpeed; //Forward speed != than backward speed
+
+        Vector3 movement = new Vector3(xAxis, 0F, zAxis);
+        //X is the strafe and Z is forward/backward
+
+        this.transform.Translate(movement); //Making the move
+    }
+
+    #endregion
 
 }
