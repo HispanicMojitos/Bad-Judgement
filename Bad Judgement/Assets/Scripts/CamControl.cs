af50bfd058beacdcf688cc8fc2ad0f51e9cfcfd5
Terraxel99 <36198830+Terraxel99@users.noreply.github.com>
2018-02-25 19:10:31 +0100

    Free camera mode almost done
    I worked on the camera free mode. It works well in vertical but I've problems in horizontal so it's not really finished.

--- Bad Judgement/Assets/Scripts/CamControl.cs
+++ Bad Judgement/Assets/Scripts/CamControl.cs
@@ -6,18 +6,25 @@ public class CamControl : MonoBehaviour
 {
     #region Members
 
-    private Camera cam; //Camera (it's a children component, this script has to be applied to the player !!!!!)
+    private Camera cam; //Current camera (it's a children component, this script has to be applied to the player !!!!!)
+    [SerializeField] private Camera normalCam; //Normal camera which isn't free moveable
+    [SerializeField] private Camera freeCam; //Reference to the free cam that hasn't a gun attached on it. Disabled by default
 
-    private float minVertical = 60.0F;
-    private float maxVertical = -55.0F;//Maximum head angle is 55 degrees. Modifiable here.
+    [SerializeField] private float minVertical = 70.0F;
+    [SerializeField] private float maxVertical = -55.0F; //Maximum head angle is 55 degrees. Modifiable here.
 
-    private Vector3 verticalEulerVector; //Création d'un vecteur pour la rotation verticale de la caméra
+    [SerializeField] private float maxLeftRightFreeCamAngle = 70.0F;
+
+    private Vector3 verticalEulerVector; //Vector to handle vertical rotation of the camera
+    private Vector3 horizontalEulerVector; //Vector to handle the horizontal rotation of the camera (IF FREE CAMERA MODE ACTIVE)
 
     #endregion
     
     #region Properties
 
     public bool isVerticalAxisInverted { get; private set; } //For you LERUTH
+
+    public bool isFreeCamActive { get; private set; }
 
     public float horizontalSensitivity { get; set; }
     public float verticalSensitivity { get; set; }
@@ -29,15 +36,16 @@ public class CamControl : MonoBehaviour
     // Use this for initialization
     void Start()
     {
-        cam = Camera.main; //Starting with the main cam which is the FPS one
+        cam = normalCam; //Starting with the main cam which is the FPS one
                            //As project I'd like to put a second cam which could be changed to TPS if we press a button
-       
+
         //Initializing properties :
         this.isVerticalAxisInverted = false;
         this.horizontalSensitivity = 6.0F;
         this.verticalSensitivity = 6.0F;
 
         verticalEulerVector = Vector3.zero;
+        horizontalEulerVector = Vector3.zero;
     }
 
 
@@ -47,10 +55,16 @@ public class CamControl : MonoBehaviour
         float yGameAxis = Input.GetAxis("Mouse X"); //The horizontal (X) mouse axis matches with the ingame y rotation axis
         float xGameAxis = Input.GetAxis("Mouse Y"); //The vertical (Y) mouse axis matches with the ingame x rotation axis
 
-        if (yGameAxis != 0) MoveCamHoriz(yGameAxis); //Rotating the player on Y axis (horizontally)
+        if (yGameAxis != 0)
+        {
+            if (isFreeCamActive) MoveCamHoriz(yGameAxis); //Rotating the camera because of freecam mode
+            else MoveCamHoriz(yGameAxis); //Rotating the player on its Y-axis.
+        }
         MoveCamVertically(xGameAxis); //Moving the camera vertically (X axis)
-        
-        
+
+        if (Input.GetKeyDown(KeyCode.LeftAlt)) ChangeFreeCamOrNot();
+        //Setting Camera to free if alt is pressed, reverting action by pressing a second time
+
         if (Input.GetKey(KeyCode.LeftAlt) && Input.GetKeyDown(KeyCode.Mouse0)) this.InvertAxis();
         //If LeftAlt + LeftClick => Inverting Y axis
     }
@@ -69,8 +83,8 @@ public class CamControl : MonoBehaviour
     {
         this.cam.transform.localEulerAngles = verticalEulerVector; //Refreshing cam movement every frame
 
-        float verticalRotation = xGameAxis * verticalSensitivity;
-        verticalEulerVector.x += verticalRotation; //Adding the mouse axis to the actual
+        float verticalRotation = xGameAxis * verticalSensitivity; //Applying sensitivity
+        verticalEulerVector.x += verticalRotation; //Adding the mouse axis to the actual rotation
 
         if (verticalEulerVector.x >= minVertical) verticalEulerVector.x = minVertical;
         if (verticalEulerVector.x <= maxVertical) verticalEulerVector.x = maxVertical;
@@ -81,6 +95,39 @@ public class CamControl : MonoBehaviour
         yGameAxis *= horizontalSensitivity; //Applying vertical sensitivity
 
         this.transform.Rotate(0F, yGameAxis, 0F); //Horizontal rotate (applied to the player)
+    }
+
+    //WIP :
+    private void MoveCamHorizIfFreeCam(float yGameAxis)
+    {
+        this.cam.transform.localEulerAngles = horizontalEulerVector; //Refreshing camera horizontal rotation every frame
+
+        float horizontalRotation = yGameAxis * horizontalSensitivity; //Applying sensitivity
+        horizontalEulerVector.y += horizontalRotation; //Adding the mouse rotation to the actual rotation
+
+        //Clamping cam to a certain angle
+        //if (verticalEulerVector.y >= maxLeftRightFreeCamAngle) verticalEulerVector.y = maxLeftRightFreeCamAngle;
+        //if (verticalEulerVector.y <= -(maxLeftRightFreeCamAngle)) verticalEulerVector.y = -maxLeftRightFreeCamAngle;
+    }
+
+    private void ChangeFreeCamOrNot()
+    {
+        if (isFreeCamActive) //If we want to have the normal camera
+        {
+            this.normalCam.enabled = true;
+            this.cam = normalCam;
+            this.freeCam.enabled = false;
+
+            this.isFreeCamActive = false;
+        }
+        else //If we want to have the free camera mode
+        {
+            this.freeCam.enabled = true;
+            this.cam = freeCam;
+            this.normalCam.enabled = false;
+
+            this.isFreeCamActive = true;
+        }
     }
 
     #endregion
