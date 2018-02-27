ae7e117dcaba55e8c3de40d6fd3085bc5651232e
BelgianSoviet <andrewsleruth@gmail.com>
2018-02-27 15:33:46 +0100

    Added rigidbody to the guard to be able to kill him
    Martin this causes the guard to fall, so I'm gonna help you out with how to make him stay straight

--- Bad Judgement/Assets/Scripts/Target.cs
+++ Bad Judgement/Assets/Scripts/Target.cs
@@ -11,10 +11,8 @@ public class Target : MonoBehaviour
     public void TakeDamage(float amount)
     {
         health -= amount;
-        if (health <= 0f) // we create a script that destroys an object when its health is equal or lower than 0
-        {
-            Die();
-        }
+        if (health <= 0f) Die();
+        // we create a script that destroys an object when its health is equal or lower than 0
     }
 
     void Die()
