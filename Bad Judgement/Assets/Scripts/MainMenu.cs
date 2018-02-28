ebf5859e6656dd0ef3aa70e36409e17368555a8f
Terraxel99 <36198830+Terraxel99@users.noreply.github.com>
2018-02-28 20:41:34 +0100

    Maine (state) ménou
    I did a fucking main menu for our game. Hope you'll like it.
    
    I know there's a bug when we press play => The light doesn't load on the game scene, but I'm going to try to solve it.
    
    Good evening, folks.

---
+++ Bad Judgement/Assets/Scripts/MainMenu.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    public void LaunchGame() //Has to be public. Thanks.
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+        //We launch the next scene in the SceneManager order
+    }
+
+    public void Quit() //Has to be public. Thanks.
+    {
+        Application.Quit(); //Quit application //Need so much detail due to the difficulty of understanding that method call
+    }
+}
