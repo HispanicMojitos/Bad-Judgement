818c2d13adf4ded4aa7e3e4fb4e06457a1ea670d
BelgianSoviet <andrewsleruth@gmail.com>
2018-03-02 16:37:11 +0100

    reload works now

--- DevDoc/How to make a shooting script/Target.cs
+++ DevDoc/How to make a shooting script/Target.cs
@@ -1,23 +1,23 @@
-using System.Collections;
+﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Target : MonoBehaviour {
-// Now we need a target script to recieve damage and other stuff
-	public float health = 50f;
+public class Target : MonoBehaviour
+{
+    // Now we need a target script to recieve damage and other stuff
+    [SerializeField]
+    private float health = 50f;
 
-	public void TakeDamage (float amount)
-	{
-		health -= amount;
-		if(health <= 0f) // we create a script that destroys an object when its health is equal or lower than 0
-		{
-			Die();
-		}
-	}
+    public void TakeDamage(float amount)
+    {
+        health -= amount;
+        if (health <= 0f) Die();
+        // we create a script that destroys an object when its health is equal or lower than 0
+    }
 
-	void Die()
-	{
-		Destroy(gameObject);
-	}
+    void Die()
+    {
+        Destroy(gameObject);
+    }
 
 }
