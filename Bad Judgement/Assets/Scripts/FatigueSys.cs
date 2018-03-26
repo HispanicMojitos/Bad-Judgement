994c1b63dac0b79d331c5c9a5024c9898e3d4274
Terraxel99 <36198830+Terraxel99@users.noreply.github.com>
2018-03-26 14:51:05 +0200

    Error fixed and regions added

--- Bad Judgement/Assets/Scripts/FatigueSys.cs
+++ Bad Judgement/Assets/Scripts/FatigueSys.cs
@@ -7,43 +7,47 @@ using UnityEngine;
 
 public class FatigueSys
 {
-    
-
     public float fatigue = 0.0f;
     public float maxFatigue = 1000.0f;
 
     private const float fatigueIncrease = 1.0f;
     private const float fatigueDecrease = 0.5f;
 
+    #region Méthodes de fatigue
+
     public void Running()
     {
         fatigue = Mathf.Clamp(fatigue + (fatigueIncrease), 0.0f, maxFatigue); //Rajoute de la fatigue en l'empechant de descendre en dessous de 0 et au dessus de 1000        
     }
+
     public void Walking()
     {
-        fatigue = Mathf.Clamp((fatigue + (fatigueIncrease))/5, 0.0f, maxFatigue);      
+        fatigue = Mathf.Clamp((fatigue + (fatigueIncrease)) / 5f, 0.0f, maxFatigue);
     }
-    public void Jumping()             
+
+    public void Jumping()
     {
         fatigue += 50.0f;
     }
+
     public void crouchWalking()                                                        //Augmente la fatigue quand il avance accroupi
     {
-        fatigue = Mathf.Clamp((fatigue + (fatigueIncrease)) * 1.5, 0.0f, maxFatigue);
+        fatigue = Mathf.Clamp((fatigue + (fatigueIncrease)) * 1.5f, 0.0f, maxFatigue);
     }
+
     public void Throw()                                                                //Fatigue sur le lancer de grenade
     {
         fatigue += 25.0f;
     }
 
-
     public void Crouched()                                                             //Diminue la fatigue quand accroupi
     {
         if (fatigue >= 100.0f)
         {
-            fatigue = Mathf.Clamp((fatigue - (fatigueIncrease)) / 1.5, 0.0f, maxFatigue);
+            fatigue = Mathf.Clamp((fatigue - (fatigueIncrease)) / 1.5f, 0.0f, maxFatigue);
         }
     }
+
     public void Idle()
     {
         if (fatigue >= 100.0f)
@@ -52,6 +56,9 @@ public class FatigueSys
         }
     }
 
+    #endregion
+
+    #region Méthodes booléennes
 
     public bool isAbleToJump()
     {
@@ -63,25 +70,30 @@ public class FatigueSys
         if (this.fatigue < 750.0F) return true;
         else return false;
     }
+
     public bool isAbleToHeal()                                  //Empeche de regen de la vie si trop fatigué
     {
         if (this.fatigue < 750.0F) return true;
         else return false;
     }
+
     public bool isAbleToThrow()                                 //Empeche de lancer des grenades
     {
         if (this.fatigue < 800.0f) return true;
         else return false;
     }
+
     public bool isAbleToAim()                                   //Empeche de viser en 1ere personne
     {
         if (this.fatigue < 900.0f) return true;
         else return false;
     }
+
     public bool gunSway()                                      //Pour les problemes de visees
     {
         if (this.fatigue > 350.0f) return true;
-        else return false; 
-    }
+        else return false;
+    } 
 
+    #endregion
 }
