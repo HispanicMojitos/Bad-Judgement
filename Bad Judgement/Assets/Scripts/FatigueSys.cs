2b258448579e35fcea5501aa2f7fdb3a4d094502
Terraxel99 <36198830+Terraxel99@users.noreply.github.com>
2018-03-26 10:04:58 +0200

    Intro to exhaust system
    After a certain amount of time, we can't run. Didn't implement the exhaust recovery

--- Bad Judgement/Assets/Scripts/FatigueSys.cs
+++ Bad Judgement/Assets/Scripts/FatigueSys.cs
@@ -12,36 +12,30 @@ public class FatigueSys
     public float fatigue = 0.0f;
     public float maxFatigue = 1000.0f;
 
-    public bool running;
-    public bool jumping;
-    public bool idle;
-
     private const float fatigueIncrease = 1.0f;
     private const float fatigueDecrease = 0.5f;
 
     public void Running()
     {
-        if (running)
-        {
-            fatigue = Mathf.Clamp(fatigue + (fatigueIncrease * Time.deltaTime), 0.0f, maxFatigue); //Rajoute de la fatigue en l'empechant de descendre en dessous de 0 et au dessus de 1000
-        }
+        fatigue = Mathf.Clamp(fatigue + (fatigueIncrease), 0.0f, maxFatigue); //Rajoute de la fatigue en l'empechant de descendre en dessous de 0 et au dessus de 1000        
     }
     public void Jumping()             
     {
-        if ( jumping)
-        {
-            fatigue += 50.0f;
-        }
+        fatigue += 50.0f;
     }
+
     public void Idle()
     {
-        if (idle && fatigue >= 100.0f)
+        if (fatigue >= 100.0f)
         {
-            fatigue = Mathf.Clamp(fatigue - (fatigueDecrease * Time.deltaTime), 0.0f, maxFatigue); //Réduit de la fatigue en l'empechant de descendre en dessous de 0 et au dessus de 1000
+            fatigue = Mathf.Clamp(fatigue - (fatigueDecrease), 0.0f, maxFatigue); //Réduit de la fatigue en l'empechant de descendre en dessous de 0 et au dessus de 1000
         }
     }
     
-
-    
+    public bool isAbleToRun()
+    {
+        if (this.fatigue < 750.0F) return true;
+        else return false;
+    }
 
 }
