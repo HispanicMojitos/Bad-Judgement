2126f037317346536a2a7eadf9fb32d9de95b594
BelgianSoviet <andrewsleruth@gmail.com>
2018-04-03 17:42:58 +0100

    WIP

---
+++ Bad Judgement/Assets/Scripts/AvatarCreator.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AvatarCreator : MonoBehaviour {
+
+    public static GameObject go;
+    public static HumanDescription human;
+    Avatar avatar;
+    Animator anim;
+	// Use this for initialization
+	void Start () {
+        go = this.gameObject;
+        avatar = AvatarBuilder.BuildHumanAvatar(go, human);
+        avatar.name = "Jeff";
+        anim = go.GetComponent<Animator>();
+        anim.avatar = avatar;
+        }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
