49dd018e2bbe57465a64ca4884f103160086f9a1
QuinzelHJ <36204956+QuinzelHJ@users.noreply.github.com>
2018-04-29 12:10:21 +0200

    interaction
    script for the money's done
    It works but i have to change things with my money prefab

---
+++ Bad Judgement/Assets/Scripts/Player/TakeMoney.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TakeMoney : MonoBehaviour {
+
+    // Use this for initialization
+
+    public Text moneyText;
+
+    void Start()
+    {
+
+        moneyText.text = "";
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+
+        Vector3 direction = transform.TransformDirection(Vector3.forward) * 100;
+        RaycastHit hit;
+
+        if ((Physics.Raycast(transform.position, direction, out hit, 3f) && hit.transform.CompareTag("Money") && Vector3.Distance(transform.position, hit.transform.position) < 10))
+        {
+            moneyText.enabled = true;
+            moneyText.text = "Press F to take the money";
+
+            if (Input.GetKeyDown(KeyCode.F))
+            {
+                Destroy(hit.transform.gameObject);
+                moneyText.enabled = false;
+            }
+        }
+        else if (moneyText.enabled == true) moneyText.enabled = false;
+    }
+}
