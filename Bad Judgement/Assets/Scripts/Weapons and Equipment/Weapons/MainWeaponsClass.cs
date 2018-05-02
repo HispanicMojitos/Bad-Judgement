0046fba26ebff878ce44ee7b2d9408bdeae3a78d
BelgianSoviet <34479138+BelgianSoviet@users.noreply.github.com>
2018-05-02 12:19:38 +0200

    changed folder name and started Weapon script

---
+++ Bad Judgement/Assets/Scripts/Weapons and Equipment/Weapons/MainWeaponsClass.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Threading;
+
+public class MainWeaponsClass : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
