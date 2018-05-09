c7b39ebdd3e42fc957a6cad6c02631f0a3ad9542
Terraxel99 <la170470@student.helha.be>
2018-05-09 18:41:54 +0200

    Minor loadout update

--- Bad Judgement/Assets/Scripts/Weapons and Equipment/PlayerLoadout.cs
+++ Bad Judgement/Assets/Scripts/Weapons and Equipment/PlayerLoadout.cs
@@ -6,14 +6,38 @@ public class PlayerLoadout : MonoBehaviour
 {
     public List<Grenade> grenades { get; private set; }
     public ProtectionEquipment protection { get; private set; }
+
+    public int selectedItem { get; private set; }
+    public int maxSelectedItem { get; private set; }
   
     private void Start()
     {
-
+        selectedItem = 0;
     }
 
     private void Update()
     {
-
+        if(!UIScript.gameIsPaused)
+        {
+            
+        }
     }
+
+    #region Equipment
+
+    private void EquipmentSelection()
+    {
+        var mouseScrollInput = Input.GetAxis("Mouse ScrollWheell");
+
+        if (mouseScrollInput > 0)
+        {
+            if(selectedItem > 0) selectedItem--;
+        }
+        else if (mouseScrollInput < 0)
+        {
+            if (selectedItem < maxSelectedItem) selectedItem++;
+        }     
+    }
+
+    #endregion
 }
