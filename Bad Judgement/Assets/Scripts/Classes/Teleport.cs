68f30d8cc182252a5404f93fd494d8d81098e301
Rarbag <la162294@student.helha.be>
2018-05-13 23:54:23 +0200

    scene changing

---
+++ Bad Judgement/Assets/Scripts/Classes/Teleport.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Teleport : MonoBehaviour {
+
+    [SerializeField] private GameObject Grenada;
+
+    private MovingTestForward Grenade;
+
+   
+	
+	void Start ()
+    {
+        Grenade = Grenada.GetComponent<MovingTestForward>();
+	}
+	
+	
+	void Update ()
+    {
+        teleportToVan();
+	}
+    void teleportToVan()
+    {
+        if (Grenade.leanLeft.activeInHierarchy == false && Grenade.collision == 20)
+        {
+            SceneManager.LoadScene("Van",LoadSceneMode.Single);
+        }
+    }
+
+    void teleportToMissionOne()
+    {
+        SceneManager.LoadScene("Mission 1", LoadSceneMode.Single);
+    }
+}
