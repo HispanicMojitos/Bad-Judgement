37cf54cab1eacb9791e085d23694c16f41b9ee5f
Terraxel99 <la170470@student.helha.be>
2018-05-13 18:58:34 +0200

    Mini OS (WIP)

---
+++ Bad Judgement/Assets/Scripts/Classes/ChooseLoadout.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+public class ChooseLoadout
+{
+    WeaponsDataBase weaponsDb;
+
+    public List<PrimaryWeapon> allPrimaryWeapons { get; private set; }
+    public List<SecondaryWeapon> allSecondaryWeapons { get; private set; }
+
+    #region Chosen things
+
+    public static readonly string directoryPath = "Loadout";
+    public static readonly string primaryWeaponPath = @"Loadout\primaryWp";
+    public static readonly string secondaryWeaponPath = @"Loadout\secondaryWp";
+
+    public int? actualPrimaryWeaponSelected { get; private set; }
+    public int? actualSecondaryWeaponSelected { get; private set; }
+
+    #endregion
+
+    public ChooseLoadout()
+    {
+        weaponsDb = new WeaponsDataBase();
+
+        allPrimaryWeapons = weaponsDb.LoadPrimary();
+        allSecondaryWeapons = weaponsDb.LoadSecondary();
+
+        actualPrimaryWeaponSelected = null;
+        actualSecondaryWeaponSelected = null;
+    }
+
+    #region Methods
+
+    
+    #endregion
+}
